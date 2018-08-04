53e38ec06f86190972784fde1d6986101b1ccfcd
Peter Nicholls <peterwnicholls@gmail.com>
2018-08-04 19:30:02 +1000

    Fixed sidekiq initilizer

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-:concurrency: 7
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], id: nil }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], id: nil }
-end
